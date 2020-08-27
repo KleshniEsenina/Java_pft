4e766d6a7dcd06b8d3711de9459a92dd09e53429
KleshniEsenina <whiteonix@mail.ru>
2020-08-27 17:23:06 +0300

    Точка возврата.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/point/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/point/Distance.java
@@ -4,11 +4,8 @@ import ru.stqa.pft.sandbox.Square;
 
 public class Distance {
   public static void main(String[] args) {
-    Point p = new Point();
-    p.x1 = 5;
-    p.y1 = 10;
-    p.x2 = 8;
-    p.y2 = 14;
+    Point p = new Point(5,8,10,14);
+
     double x = Math.sqrt(area(p)*area(p)+area1(p)*area1(p));
      System.out.println("Расстояние между точками = " + x);
   }
