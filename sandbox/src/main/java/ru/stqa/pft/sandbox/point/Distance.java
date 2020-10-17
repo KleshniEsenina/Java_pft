dc5532aa5451e265d58d4a03ffeac219ca4c6fb3
KleshniEsenina <whiteonix@mail.ru>
2020-10-17 18:33:15 +0300

    Выполнил задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/point/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/point/Distance.java
@@ -5,8 +5,8 @@ public class Distance {
   public static void main(String[] args) {
     Point p1 = new Point(0, 0);
     Point p2 = new Point(3, 4);
-    System.out.println(p1.distance(p2));
-     }
+    System.out.println("Расстояние между точками P1 и P2 равно " + p1.distance(p2));
+         }
 
 }
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -2,11 +2,22 @@ package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.point.Point;
 
 public class SquareTests {
   @Test
   public void testArea(){
     Square s = new Square(5);
     Assert.assertEquals(s.area(),25.0);
+
+    Point p = new Point(0,0);
+    Point p1 = new Point(3,4);
+    Assert.assertEquals(p.distance(p1),5.0);
+
+    assert p.distance(p1)==5;
+
+        Assert.assertEquals(p.distance(p1),6.0);
+    //Для примера сделал 1 неправильный
   }
+
 }
